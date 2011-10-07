db1ae4a32aef962ba4a027ecf66dc33b492ce9f1
tengfei <tengfei@tengfei-T510.(none)>
2011-10-06 21:20:11 -0500

    revise tracks

--- R/tracks-plot.R
+++ R/tracks-plot.R
@@ -1,59 +1,3 @@
-##' In most genome browsers, they all have such a view that including
-##' many tracks, could be any anntation data along genomic
-##' coordinate. So we try to provide a convenient constructor for
-##' building tracks, which here in this package is simply vertically
-##' binding of several plots. It's essentially a
-##' \code{grid.arrange}. So if users want to have more delicate
-##' control over their tracks, they need manipulate the graphics in
-##' ggplot2 level or grid levels.
-##'
-##' \code{tracks} function has some extra features and limitations
-##' compare to \code{grid.arrange}.
-##' \itemize{
-##' \item{}{
-##' Always sitting on genomic or protein space.
-##' }
-##' \item{}{
-##' Always using ncol = 1 as default arguments.
-##' }
-##' \item{}{
-##' For now, since the unbalanced legend and labels in ggplot2 has
-##'   been solved (maybe just I haven't found such features). We simply
-##'   remove legend and y axis labels to make sure all tracks are aligned
-##'   exactly in the same way.
-##' }
-##' \item{}{
-##' Remove the x-axis for most track except the last one.
-##' } 
-##' \item{}{
-##' Does the ajustment of margins for you automatically.
-##' }
-##' \item{}{
-##'   Doesn't like \code{qplot}, tracks doesn't return \code{ggplot}
-##'   object. so processing your plot before you pass them to
-##'   \code{tracks}.
-##' }
-##' }
-##' @title Tracks for genomic graphics
-##' @param ... Plots of class ggplot2, trellis, or grobs, and valid
-##'          arguments to grid.layout.
-##' @param check.xlim Default is TRUE, make sure all tracks are on the
-##' same scale. if FALSE, just like common grid.arrange function.
-##' @param legend Default is FALSE, remove legend, this make sure all
-##' tracks are aligned exactly based on the same position.
-##' @param xlim Limits on x.
-##' @param ylim Limits on y.
-##' @seealso \code{\link{grid.arrange}}
-##' @return return a frame grob; side-effect (plotting) if plot=T.
-##' @author Tengfei Yin
-##' @examples
-##' library(BSgenome.Hsapiens.UCSC.hg19)
-##' gr <- GRanges("chr1", IRanges(5e7, 5e7+50))
-##' p1 <- qplot(Hsapiens, name = gr, geom = "text")
-##' p2 <- qplot(Hsapiens, name = gr, geom = "point")
-##' p3 <- qplot(Hsapiens, name = gr, geom = "segment")
-##' p4 <- qplot(Hsapiens, name = gr, geom = "rectangle")
-##' tracks(p1, p2, p3, p4)
 tracks <- function(..., check.xlim = TRUE,
                    legend = FALSE,
                    xlim, ylim){
@@ -71,19 +15,17 @@ tracks <- function(..., check.xlim = TRUE,
   grobs <- dots[!not.grobnames]
   
   if(missing(xlim)){
-    lst <- lapply(grobs, function(obj){
-      x <- eval(obj$layers[[1]]$mapping$x, obj$data)
-      if(!is.null(x))
-        data.frame(xmin = min(x),xmax = max(x))
-      else
-        data.frame(xmin = min(obj$data$start),xmax = max(obj$data$end))
-    })
-    res <- do.call("rbind", lst)
-    xlim <- c(min(res$xmin), max(res$xmax))
-    extr <- 0.05*(diff(xlim))
-    xlim <- c(c(xlim[1] - extr), c(xlim[2] + extr))
+  lst <- lapply(grobs, function(obj){
+    res <- getLimits(obj)
+    data.frame(xmin = res$xlim[1], xmax = res$xlim[2])
+  })
+  res <- do.call(rbind, lst)
+  xlim <- c(min(res$xmin), max(res$xmax))
+  xlim <- scales::expand_range(xlim, mul = 0.05)
+  }else{
+    s <- scale_x_continuous(limits = xlim)    
   }
-  s <- scale_x_continuous(limits = xlim)
+  
   if(!missing(ylim))
     s <- scale_y_continuous(limits = ylim)
   ## need fix legend
--- R/utils.R
+++ R/utils.R
@@ -18,35 +18,110 @@ rangesCentric <- function(gr, which, id.name){
 }
 
 getLimits <- function(obj){
-  if(is.null(obj$mapping)){
-    x <- eval(obj$layers[[1]]$mapping$x, obj$data)
-    y <- eval(obj$layers[[1]]$mapping$y, obj$data)  
-  }else{
+  if(!is.null(obj$mapping$x) && length(obj$data))
     x <- eval(obj$mapping$x, obj$data)
-    y <- eval(obj$mapping$y, obj$data)  
-  }
-  if(is.null(obj$coordinates$limits$x)){
-    if(!is.null(x))
-      limx <- c(min(x), xmax = max(x))
-    else
-      limx <- c(min(obj$data$start), xmax = max(obj$data$end))
-  }else{
-    limx <- obj$coordinates$limits$x
-  }
+  else
+    x <- NULL
+  
+  if(!is.null(obj$mapping$y) && length(obj$data))
+    y <- eval(obj$mapping$y, obj$data)
+  else
+    y <- NULL
+  
+  if(!is.null(obj$mapping$xmin) && length(obj$data))
+    xmin <- eval(obj$mapping$xmin, obj$data)
+  else
+    xmin <- NULL
+  
+  if(!is.null(obj$mapping$ymin) && length(obj$data))
+    ymin <- eval(obj$mapping$ymin, obj$data)
+  else
+    ymin <- NULL
+  
+  if(!is.null(obj$mapping$xmax) && length(obj$data))
+    xmax <- eval(obj$mapping$xmax, obj$data)
+  else
+    xmax <- NULL
+  
+  if(!is.null(obj$mapping$ymax) && length(obj$data))
+    ymax <- eval(obj$mapping$ymax, obj$data)
+  else
+    ymax <- NULL
+  
+  if(!is.null(obj$mapping$xend) && length(obj$data))
+    xend <- eval(obj$mapping$xend, obj$data)
+  else
+    xend <- NULL
+  
+  if(!is.null(obj$mapping$yend) && length(obj$data))
+    yend <- eval(obj$mapping$yend, obj$data)
+  else
+    yend <- NULL
 
-  if(is.null(obj$coordinates$limits$y)){
-    if(!is.null(y))
-      limy <- c(min(y), xmax = max(y))
-    else
-      limy <- c(min(obj$data$start), xmax = max(obj$data$end))
+  if(length(obj$layer)>1){
+    l.res <- getLimitsFromLayer(obj$layer)
   }else{
-    limx <- obj$coordinates$limits$y
+    l.res <- NULL
   }
-  names(limx) <- NULL
-  names(limy) <- NULL
-  list(xlim = limx, ylim = limy)
+  
+  list(xlim = c(min(c(l.res$xmin, x, xmin)),
+         max(c(l.res$xmax, x, xmax, xend))),
+       ylim = c(min(c(l.res$ymin, y, ymin)),
+         max(c(l.res$ymax, y, ymax, yend))))
 }
 
+getLimitsFromLayer <- function(layers){
+  lst <- lapply(layers, function(layer){
+    if(length(layer$data)){
+    if(!is.null(layer$mapping$x))
+      x <- eval(layer$mapping$x, layer$data)
+    else
+      x <- NULL
+    
+    if(!is.null(layer$mapping$y))
+      y <- eval(layer$mapping$y, layer$data)
+    else
+      y <- NULL
+    
+    if(!is.null(layer$mapping$xmin))
+      xmin <- eval(layer$mapping$xmin, layer$data)
+    else
+      xmin <- NULL
+    
+    if(!is.null(layer$mapping$ymin))
+      ymin <- eval(layer$mapping$ymin, layer$data)
+    else
+      ymin <- NULL
+    
+    if(!is.null(layer$mapping$xmax))
+      xmax <- eval(layer$mapping$xmax, layer$data)
+    else
+      xmax <- NULL
+    
+    if(!is.null(layer$mapping$ymax))
+      ymax <- eval(layer$mapping$ymax, layer$data)
+    else
+      ymax <- NULL
+    
+    if(!is.null(layer$mapping$xend))
+      xend <- eval(layer$mapping$xend, layer$data)
+    else
+      xend <- NULL
+    
+    if(!is.null(layer$mapping$yend))
+      yend <- eval(layer$mapping$yend, layer$data)
+    else
+      yend <- NULL
+    res <- data.frame(xmin = min(c(x, xmin)), xmax = max(c(x, xmax, xend)),
+               ymin = min(c(y, ymin)), ymax = max(c(y, ymax, yend)))
+  }else{
+    res <- NULL
+  }
+  })
+  lst <- lst[!is.null(lst)]
+  res <- do.call("rbind", lst)
+  res
+}
 
 
 

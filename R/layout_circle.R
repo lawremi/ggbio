802fc97f3dd86ce5cdcaf6547eafa154468cb911
tengfei <tengfei@tengfei-ThinkPad-T510>
2012-01-19 02:38:56 -0600

    arangment

--- R/layout_circle.R
+++ R/layout_circle.R
@@ -62,36 +62,6 @@ gr2circle <- function(obj, x = ".biovizBase.start", y,
 
 ## reference:http://groups.google.com/group/ggplot2/browse_thread/thread/72403c6997b79c3b?pli=1
 
-blank_opts <- function(size=12) 
-{ 
-  o = list(axis.line=theme_blank(), 
-    axis.text.x=theme_blank(), 
-    axis.text.y=theme_blank(), 
-    axis.ticks=theme_blank(), 
-    axis.ticks.length=unit(0.3, "lines"), 
-    axis.ticks.margin=unit(0.5, "lines"), 
-    axis.title.x=theme_blank(), 
-    axis.title.y=theme_blank(), 
-    legend.background=theme_rect(fill="white", colour=NA), 
-    legend.key=theme_rect(colour="white"), 
-    legend.key.size=unit(1.2, "lines"), 
-    legend.position="right", 
-    legend.text=theme_text(size=size*0.8), 
-    legend.title=theme_text(size=size*0.8, face="bold", 
-      hjust=0), 
-    panel.background=theme_blank(), 
-    panel.border=theme_blank(), 
-    panel.grid.major=theme_blank(), 
-    panel.grid.minor=theme_blank(), 
-    panel.margin=unit(0, "lines"), 
-    plot.background=theme_blank(), 
-    plot.margin=unit(c(1, 1, 0.5, 0.5), "lines"), 
-    plot.title=theme_text(size=size*1.2), 
-    strip.background=theme_rect(fill="grey90", 
-      colour="grey50"), 
-    strip.text.x=theme_text(size=size*0.8), 
-    strip.text.y=theme_text(size=size*0.8, angle=-90)) 
-  return(structure(o, class="options"))}   
 
 ## "link" and "ribbon" requie a special data structure
 ## 1. we could implement it as GRangesList with no direction specification

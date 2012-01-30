d8fc54c7aa20c59360db7844a3dadda0a1855886
tengfei <tengfei@tengfei-ThinkPad-T510>
2012-01-30 09:09:56 -0600

    update singleChromosome default

--- R/plotStackedOverview.R
+++ R/plotStackedOverview.R
@@ -72,20 +72,17 @@ plotStackedOverview <- function(obj,
 }
 
 
-plotSingleChrom <- function(obj, subchr, zoom.region, xlabel = FALSE){
+plotSingleChrom <- function(obj, subchr, zoom.region, xlabel = FALSE,
+                            xlab, ylab, main){
   ## do we need subchr here
   if(!missing(subchr)){
-    keepSeqlevels(obj, "chr1")
     obj <- obj[seqnames(obj) == subchr]
-    obj
     ## seqlevels(obj) <- sortChr(unique(as.character(seqnames(obj))))
-    seqlevels(obj)
-    names(seqlevels(obj))
   }
   if(length(unique(as.character(seqnames(obj))))>1)
     stop("Mulptiple chromosome information found")
-  p <- plotOverview(obj, cytoband = TRUE)
-  p <- p + xlab("") + opts(legend.position = "none")
+  p <- plotStackedOverview(obj, cytoband = TRUE)
+  p <- p + opts(legend.position = "none") + ggplot2::xlab("")
   if(!missing(zoom.region)){
     if(length(zoom.region) != 2)
       stop("zoom.region must be a numeric vector of length 2")
@@ -93,11 +90,18 @@ plotSingleChrom <- function(obj, subchr, zoom.region, xlabel = FALSE){
                           x2 = zoom.region[2],
                           seqnames = unique(as.character(seqnames(obj))))
     p <- p + geom_rect(data = zoom.df, aes(xmin = x1,
-                         xmax = x2, ymin = -1, ymax = 11), color = "red", fill = NA)
+                         xmax = x2, ymin = -3, ymax = 13), color = "red", fill = NA)
   }
   if(!xlabel)
-    p <- p + opts(axis.text.x = theme_blank(),
-                  axis.title.x=theme_blank())
+    p <- p + opts(axis.text.x = theme_blank())
+  p <- p + theme_alignment(grid = FALSE, label = TRUE, border = FALSE) +
+    scale_y_continuous(breaks = 5, label = subchr)
+  if(!missing(xlab))
+    p <- p + ggplot2::xlab(xlab)
+  if(!missing(ylab))
+    p <- p + ggplot2::ylab(ylab)
+  if(!missing(main))
+    p <- p + opts(title = main)
   p
 }
 

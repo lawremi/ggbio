01073db2105a74c6a045bb46f2a696eabdd98549
tengfei <tengfei@tengfei-ThinkPad-T510>
2012-01-30 09:11:43 -0600

    More features to linked plot, significant color, bug fix, annotation tracks,...

--- R/plotRangesLinkedToData.R
+++ R/plotRangesLinkedToData.R
@@ -1,10 +1,23 @@
-plotRangesLinkedToData <- function(data, stat.col, stat.label, solid.size = 1.3,...,
+## TODO: exons label
+## check (done)
+## significant
+##  log y
+plotRangesLinkedToData <- function(data, stat.col, stat.label,
+                                   stat.ylab,
+                                   sig, sig.col = c("black", "red"),
+                                   stat.coord.trans = coord_trans(),
+                                   solid.size = 1.3,...,
                                    annotation = list(),
-                                   width.ratio = 0.8, 
+                                   width.ratio = 0.8,
+                                   track.skip = -1,
+                                   theme.stat = theme_grey(),
+                                   theme.align = theme_grey(),
                                    heights){
   
   args <- as.list(match.call(call = sys.call(sys.parent()))[-1])
-  args <- args[!names(args) %in% c("stat.col", "stat.label", "annotation", "solid.size")]
+  args <- args[!names(args) %in% c("stat.col", "stat.label", "annotation", "solid.size",
+                                   "stat.coord.trans","sig.col","stat.ylab",
+                                   "heights", "track.skip")]
 
   if(missing(heights))
       heights <- unit(c(2.5, 0.5, 1, rep(1, length(annotation))), "null")
@@ -17,15 +30,28 @@ plotRangesLinkedToData <- function(data, stat.col, stat.label, solid.size = 1.3,
   df <- as.data.frame(gr)
   ## FIXME: need to allow specified by stat.col
   ## head(df)
-  if(is.numeric(stat.col))
+
+  if(missing(stat.col) & missing(stat.label))
+    stop("stat.col or stat.label must be provided" )
+  if(!missing(stat.col)){
+    if(is.numeric(stat.col))
+      stat.col <- stat.col 
+    else
+      stop("stat.col must be numeric values")
+    if(missing(stat.label))
+      stat.label <- colnames(values(data))[stat.col]
     stat.col <- stat.col + 5
+  }else{
+    if(all(stat.label %in% colnames(values(data))))
+      stat.col <- match(stat.label, colnames(values(data))) + 5
+    else
+      stop("stat.label must be the column names of element meta data")
+  }
+
   df.new <- melt(df, measure.vars = stat.col)  
 
-  if(missing(stat.label))
-    stat.label <- stat.col
   df.new$.ggbio.group <- rep(stat.label, each = nrow(df))
   p <- ggplot(df.new)
-
   args <- args[names(args) != "data"]
   args.seg.solid <- c(list(x = substitute(x.new-wid, list(wid = wid)),
                            xend = substitute(x.new+wid, list(wid = wid)),
@@ -52,36 +78,59 @@ plotRangesLinkedToData <- function(data, stat.col, stat.label, solid.size = 1.3,
                       linetype = 3,
                       color = substitute(.ggbio.group)),
                  args.dash.a)
-
   p.stat <- p + geom_segment(data = df.dash, do.call(aes, args.dash))
-  p.stat <- p.stat  +  theme_bw() + opts(panel.grid.minor=theme_blank()) +
-    scale_colour_discrete(name = "Group")    
-
-
-
+  p.stat <- p.stat  +  theme.stat + opts(panel.grid.minor=theme_blank()) +
+    labs(colour = "group")
+  p.stat <- p.stat + stat.coord.trans
+  if(!missing(stat.ylab))
+    p.stat <- p.stat + ylab(stat.ylab)
   ## link track
   df$midpoint <- (df$start + df$end)/2
   p.link <- ggplot(df)
-  args.link <- list(x = substitute(midpoint),
-                    xend = substitute(x.new),
-                    y = 0,
-                    yend = 10)
+  if(missing(sig)){
+    args.link <- list(x = substitute(midpoint),
+                      xend = substitute(x.new),
+                      y = 0,
+                      yend = 10)
+    p.link <- p.link + geom_segment(do.call(aes, args.link)) + theme_null()    
+  }else{
+    args.link <- list(x = substitute(midpoint),
+                      xend = substitute(x.new),
+                      y = 0,
+                      yend = 10,
+                      color = as.name(sig))
+    p.link <- p.link + geom_segment(do.call(aes, args.link)) + theme_null()        
+    p.link <- p.link + scale_color_manual(values = c("FALSE" = sig.col[1],
+                                            "TRUE" = sig.col[2])) 
+  }
+  p.link <- p.link + opts(legend.position = "none")
 
-  p.link <- p.link + geom_segment(do.call(aes, args.link)) + theme_null()
-
-  grl <- GRangesList(data)
-  ## names(grl) <- "1"
-  ## p.single <- autoplot(grl) +  opts(panel.grid.minor=theme_blank()) + ylab(" ") +
-  ##   theme_bw()
-  p.single <- autoplot(grl) + theme_alignment() + scale_y_continuous(breaks = NA)
+  ## grl <- GRangesList(data)
+  if(missing(sig)){
+    ## grl
+    p.single <- autoplot(data, geom = "alignment") + theme.align + scale_y_continuous(breaks = NA)
+  }else{
+    args <- list(data = data,
+                 fill = as.name(sig),
+                 color = as.name(sig),
+                 geom = "alignment")
+   p.single <- do.call(autoplot, args) +
+     theme.align + scale_y_continuous(breaks = NA)    
+   p.single <- p.single +
+     scale_color_manual(values = c("FALSE" = sig.col[1], "TRUE" = sig.col[2])) +
+       scale_fill_manual(values = c("FALSE" = sig.col[1], "TRUE" = sig.col[2])) +
+         opts(legend.position = "none")
+}
   if(length(annotation)){
     ## annotation <- lapply(annotation, function(p) p + theme_bw())
     ## tracks(p.stat,p.link,p.single)
-    args.tracks <- c(list(p.stat, p.link, p.single),
-                     annotation, list(heights = heights))
+    args.tracks <- c(c(list(p.stat, p.link, p.single), annotation),
+                     list(heights = heights),
+                     list(track.skip = track.skip))
   }else{
     args.tracks <- c(list(p.stat, p.link, p.single),
-                     list(heights = heights))
+                     list(heights = heights),
+                     list(track.skip = track.skip))
   }
   do.call(tracks, args.tracks)
 }

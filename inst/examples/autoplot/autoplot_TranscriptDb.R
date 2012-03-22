4e7ec3ebee60eff3fd935b2ca63f513c8a678747
tengfei <tengfei@tengfei-ThinkPad-T510>
2012-03-22 01:26:39 -0500

    add some examples files

--- inst/examples/autoplot/autoplot_TranscriptDb.R
+++ inst/examples/autoplot/autoplot_TranscriptDb.R
@@ -11,10 +11,15 @@ p1
 p2 <- ggplot() + stat_gene(txdb, which = genesymbol["RBM17"], geom = "reduced_gene")
 tracks(p1, p2, heights = c(3, 1))
 
-biovizBase:::fetch(txdb, which = list(gene_id = aldoa_eg))
+
 ## by id
 library(org.Hs.eg.db)
 aldoa_eg <- org.Hs.egSYMBOL2EG$ALDOA
+res <- biovizBase:::fetch(txdb, which = list(gene_id = aldoa_eg))
+res <- exons(txdb, val = list(gene_id = aldoa_eg), columns = c("exon_id", "tx_id"))
+res
+res2 <- fortify(res)
+fortify(res[,idx])
 p1 <- ggplot() + stat_gene(txdb, which = list(gene_id = aldoa_eg))
 p2 <- ggplot() + stat_gene(txdb, which = genesymbol["ALDOA"])
 p3 <- autoplot(txdb, which = list(gene_id = aldoa_eg))

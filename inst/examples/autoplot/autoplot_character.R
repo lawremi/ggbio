f1ecce0de3e4d2b5b455e9b72e798d201c3b1591
Tengfei Yin <yintengfei@gmail.com>
2012-08-27 15:03:48 -0500

    remove github version of ggbio, the development is moved to bioconductor

--- inst/examples/autoplot/autoplot_character.R
+++
@@ -1,6 +0,0 @@
-library(ggbio)
-library(GenomicRanges)
-library(Rsamtools)
-bamfile <- "~/Datas/seqs/ENCODE/caltech/single/wgEncodeCaltechRnaSeqK562R1x75dAlignsRep1V2.bam"
-autoplot(bamfile, stat = "coverage")
-

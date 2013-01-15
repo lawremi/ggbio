4a36be7b67692965ea0b57f95d18560c70ea801b
Tengfei Yin <yintengfei@gmail.com>
2013-01-15 09:31:31 +0000

    add coord_genome file
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ggbio@72533 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/coord_genome-method.R
@@ -0,0 +1,15 @@
+## simply wrapper
+theme_genome <- function(){
+  list(facet_grid(.~seqnames),
+       theme_pack_panels(),
+       scale_x_continuous(breaks = NULL))       
+}
+
+
+setGeneric("coord_genome", function(data,...)  standardGeneric("coord_genome"))
+
+setMethod("coord_genome", "GRanges", function(data, space.skip = 0.1){
+  object <- transformToGenome(object, space.skip = space.skip)
+  object <- biovizBase:::rescaleGr(object)
+})
+

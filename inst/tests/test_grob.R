3dcef42f7d599cd589c28f9b07a70932858f84a7
Tengfei Yin <yintengfei@gmail.com>
2013-05-19 21:28:39 +0000

    add test directory
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ggbio@76733 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/tests/test_grob.R
@@ -0,0 +1,66 @@
+context("Test Grob Class")
+require(lattice)
+require(testthat)
+require(ggplot2)
+require(gridExtra)
+x = 1:3
+p1 <- qplot(x = mpg, y = wt, data = mtcars)
+p2 <- xyplot(1:10 ~ 1:10)
+require(ggbio)
+p3 <- new("GGbio", p1)
+p <- plot(1:10, 1:10)
+test_that("test grob", {
+  expect_that(p1, is_a("gg"))
+  expect_that(p2, is_a("trellis"))
+  expect_that(x, is_a("integer"))
+  expect_that(Grob(p1), is_a("Grob"))
+  expect_that(Grob(p1), is_a("ggplotGrob"))  
+  expect_that(Grob(p2), is_a("Grob"))
+  expect_that(Grob(p2), is_a("latticeGrob"))
+  expect_that(Grob(p3), is_a("Grob"))
+  expect_that(Grob(p3), is_a("ggplotGrob"))  
+  expect_that(Grob(p), throws_error())    
+  expect_that(GrobList(p1, p2), is_a("GrobList"))
+  expect_that(GrobList(p1, p2, x), gives_warning())
+  expect_that(length(GrobList(p1, p2, x)), equals(2))
+})
+
+
+context("Test Tracked Object")
+test_that("Test Tracked", {
+  obj <- new("Tracked")
+  height(obj) <- 1
+  expect_that(obj, is_a("Tracked"))
+  expect_that(height(obj), is_a("unit"))
+  expect_that(mutable(obj), is_a("logical"))
+  expect_that(fixed(obj), is_a("logical"))
+  expect_that(bgColor(obj), is_a("character"))
+  expect_that(labeled(obj), is_a("logical"))
+  expect_that(hasAxis(obj), is_a("logical"))
+})
+
+context("Test Cache Class")
+test_that("Test Cache Class", {
+  obj <- new("Cache")
+  expect_that(obj, is_a("Cache"))
+  expect_that(Cache(), is_a("Cache"))
+  cached_xlim(obj) <- 1
+  expect_that(cached_xlim(obj), equals(c(1, 1)))
+  cached_xlim(obj) <- c(1, 2, 3)
+  expect_that(cached_xlim(obj), equals(c(1, 3)))
+
+  cached_ylim(obj) <- 1
+  expect_that(cached_ylim(obj), equals(c(1, 1)))
+  cached_ylim(obj) <- c(1, 2, 3)
+  expect_that(cached_ylim(obj), equals(c(1, 3)))    
+})
+
+context("Test Plot Class")
+test_that("test Plot", {
+  expect_that(plotList(p1, p2), is_a("plotList"))
+  expect_that(plotList(p1, p2, x), gives_warning())
+  expect_that(length(plotList(p1, p2, x)), equals(2))
+  
+})
+
+

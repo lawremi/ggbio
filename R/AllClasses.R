df3a143da7bf2372e75e759d5da0cee4b7510f67
Tengfei Yin <yintengfei@gmail.com>
2013-05-19 22:34:14 +0000

    add numericORNULL
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ggbio@76737 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -7,6 +7,7 @@ setOldClass("grob")
 setClassUnion("themeORNULL", c("theme", "NULL"))
 setClassUnion("optionsORNULL", c("options", "NULL"))
 setClassUnion("numericORunit", c("numeric", "unit"))
+setClassUnion("numericORNULL", c("numeric", "NULL"))
 
 
 
@@ -14,4 +15,9 @@ setOldClass("ggplot")
 setClassUnion("ggplotORNULL", c("ggplot", "NULL"))
 setOldClass("gg")
 setClassUnion("ggORNULL", c("gg", "NULL"))
+
+setOldClass("grob")
+setOldClass("trellis")
+setOldClass("lattice")
+
 

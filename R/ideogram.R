93bd5b4410989ed66c29ed3d23ba3f498659ee72
Michael Lawrence <lawremi@iastate.edu>
2015-12-19 14:37:04 +0000

    remove plotSingleChrom
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ggbio@111661 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ideogram.R
+++ R/ideogram.R
@@ -185,8 +185,6 @@ plotIdeogram <- function(obj, subchr = NULL, zoom.region = NULL, which = NULL,
     p
 }
 
-plotSingleChrom <- .Deprecated("plotIdeogram")
-
 setMethod("+", c("Ideogram"), function(e1, e2){
     if(inherits(e2, "xlim")){
         if("chr" %in% names(attributes(e2))){

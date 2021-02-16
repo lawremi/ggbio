58cd0722e0921b24d96eaf9dc5c8966403647007
Sanchit Saini <sanchitsaini14@gmail.com>
2021-02-16 21:40:21 +0530

    Add tests for geom_bar

---
+++ tests/testthat/data.R
@@ -0,0 +1,11 @@
+require(GenomicRanges)
+N <- 100
+data <- GRanges(seqnames = sample(c("chr1", "chr2", "chr3"),
+              size = N, replace = TRUE), 
+              IRanges(start = sample(1:300, size = N, replace = TRUE),
+                      width = sample(70:75, size = N,replace = TRUE)),
+              strand = sample(c("+", "-", "*"), size = N, replace = TRUE),
+              value = rnorm(N, 10, 3), score = rnorm(N, 100, 30),
+              sample = sample(c("Normal", "Tumor"),
+              size = N, replace = TRUE),
+              pair = sample(letters, size = N, replace = TRUE))

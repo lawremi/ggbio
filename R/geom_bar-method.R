b1c752a43e3c84a9785554a32dfda0d9af7f328e
Sanchit Saini <sanchitsaini14@gmail.com>
2021-03-10 19:40:44 +0530

    Initial Clean Up in geom_bar

--- R/geom_bar-method.R
+++ R/geom_bar-method.R
@@ -3,14 +3,14 @@ setGeneric("geom_bar", function(data, ...) standardGeneric("geom_bar"))
 setMethod("geom_bar", "ANY", function(data, ...){
   ggplot2::geom_bar(data  = data, ...)
 })
+
 ## alignment should be convenient toggle with chevron...
 setMethod("geom_bar", "GRanges", function(data,..., xlab, ylab, main){
 
   args <- list(...)
   args.aes <- parseArgsForAes(args)
   args.non <- parseArgsForNonAes(args)
-  args.facets <- subsetArgsByFormals(args, facet_grid, facet_wrap)
-  facet <- .buildFacetsFromArgs(data, args.facets)
+  facet <- build_facet(data, args, facet_grid, facet_wrap)
   if(length(data)){
     if(!"y" %in% names(args.aes)){
       if("score" %in% colnames(values(data))){
@@ -23,27 +23,18 @@ setMethod("geom_bar", "GRanges", function(data,..., xlab, ylab, main){
     .y <- quo_name(args.aes$y)
     if (missing(ylab))
       ylab <- .y
-    args.aes <- args.aes[names(args.aes) != "y"]
-    
+    args.aes <- remove_args(args.aes, "y")
     args.aes$xmin <- as.name("start")
     args.aes$xmax <- as.name("end")
     args.aes$ymin <- 0
     args.aes$ymax <- as.name(.y)
     aes.res <- do.call(aes, args.aes)
-    p <- list(do.ggcall(geom_rect, c(list(data = mold(data)),
-                                     list(aes.res),
-                                     args.non)))
-
+    p <- list(do.ggcall(geom_rect, c(list(data = mold(data)), list(aes.res), args.non)))
   }else{
     p <- NULL
   }
   p <- c(p, list(facet))
-  if(missing(xlab)) 
-    xlab <- ""
-  p <- c(p, list(ggplot2::xlab(xlab)))
-  if(!missing(ylab))
-    p <- c(p, list(ylab(ylab)))
-  if(!missing(main))
-    p <- c(p, list(labs(title = main)))
+  labels <- Labels(xlab, ylab, main, fallback = c(x = ""))
+  p <- c(p, labels)
   p
 })

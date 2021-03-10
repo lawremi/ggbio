2fa0b3bb208f6aec6c7b45fae03ced1ffb078079
Sanchit Saini <sanchitsaini14@gmail.com>
2021-03-10 19:43:54 +0530

    Initial Clean Up in geom_segment

--- R/geom_segment-method.R
+++ R/geom_segment-method.R
@@ -13,58 +13,33 @@ setMethod("geom_segment", "GRanges", function(data,..., xlab, ylab, main,
   args$facets <- facets
   args.aes <- parseArgsForAes(args)
   args.non <- parseArgsForNonAes(args)
-  args.facets <- subsetArgsByFormals(args, facet_grid, facet_wrap)
-  facet <- .buildFacetsFromArgs(data, args.facets)
+  facet <- build_facet(data, args, facet_grid, facet_wrap)
   
   stat <- match.arg(stat)
-
-  if("extend.size" %in% names(args.non))
-    es <- args.non$extend.size
-  else
-    es <- 0
-  
-
+  es <- ifelse("extend.size" %in% names(args.non), args.non$extend.size, 0)
   if(length(data)){
   if(stat == "stepping"){
     grl <- splitByFacets(data, facets)
-    res <- endoapply(grl,
-                     function(dt){
-                       if("group" %in% names(args.aes))
-                         dt <- addStepping(dt, group.name = quo_name(args.aes$group),
-                                            group.selfish = group.selfish,
-                                           extend.size = es)
-                       else
-                         dt <- addStepping(dt, extend.size = es)
-                     })
-    res <- unlist(res)
-    df <- mold(res)
+    res <- endoapply(grl, make_addStepping, args.aes, group.selfish, extend.size = es)
+    df <- mold(unlist(res))
 
-    args.aes <- args.aes[!(names(args.aes) %in% c("x", "xend", "y", "yend", "data"))]
-    args.non <- args.non[!(names(args.non) %in% c("x", "xend", "yend", "yend", "data"))]
-    if("group" %in% names(args.aes))
-      gpn <- quo_name(args.aes$group)
-    else
-      gpn <- "stepping"
-    args.aes <- args.aes[names(args.aes) != "group"]
+    args.aes <- remove_args(args.aes, c("x", "xend", "y", "yend", "data"))
+    args.non <- remove_args(args.non, c("x", "xend", "yend", "yend", "data"))
+    gpn <- ifelse("group" %in% names(args), quo_name(args$group), "stepping")
+    args.aes <- remove_args(args.aes, "group")
     args.aes <- c(args.aes, list(x = substitute(start),
                                  xend = substitute(end),
                                  y = substitute(stepping),
                                  yend = substitute(stepping)))
 
-    args.aes <- args.aes[names(args.aes) != "size"]
+    args.aes <- remove_args(args.aes, "size")
     aes.res <- do.call(aes, args.aes)
-    args.res <- c(list(data = df), list(aes.res),
-                  args.non)
+    args.res <- c(list(data = df), list(aes.res), args.non)
     p <- list(do.ggcall(ggplot2::geom_segment,args.res))
     p <- .changeStrandColor(p, args.aes)
-    .df.lvs <- unique(df$stepping)
-    .df.sub <- df[, c("stepping", gpn)]
-    .df.sub <- .df.sub[!duplicated(.df.sub$stepping),]
-    if(gpn != "stepping" & group.selfish)
-      p <- c(p , list(scale_y_continuous(breaks = .df.sub$stepping,
-                                         labels = as.character(.df.sub[, gpn]))))
-    else
-      p <- c(p, list(scale_y_continuous(breaks = NULL)))
+    .df.sub <- group_df(df, gpn)
+    y_scale <- scale_y_continuous_by_group(.df.sub, gpn, group.selfish)
+    p <- c(p, y_scale)
   }
   
   if(stat == "identity"){
@@ -80,26 +55,19 @@ setMethod("geom_segment", "GRanges", function(data,..., xlab, ylab, main,
       args.aes$y <- args.aes$yend <- .y
     }
     df <- mold(data)
-    args.aes <- args.aes[names(args.aes) != "group"]
-    args.aes <- args.aes[names(args.aes) != "size"]
+    args.aes <- remove_args(args.aes, "group")
+    args.aes <- remove_args(args.aes, "size")
     
     aes.res <- do.call(aes, args.aes)
-    args.res <- c(list(data = df), list(aes.res),
-                  args.non)
+    args.res <- c(list(data = df), list(aes.res),args.non)
     p <- list(do.ggcall(ggplot2::geom_segment,args.res))
     p <- .changeStrandColor(p, args.aes)
-  }}else{
+  }
+  }else{
     p <- NULL
   }
   p <- c(list(p) , list(facet))
-
-  if(missing(xlab)) 
-    xlab <- ""
-  p <- c(p, list(ggplot2::xlab(xlab)))
-  
-  if(!missing(ylab))
-    p <- c(p, list(ggplot2::ylab(ylab)))
-  if(!missing(main))
-    p <- c(p, list(labs(title = main)))
+  labels <- Labels(xlab, ylab, main, fallback = c(x = ""))
+  p <- c(p, labels)
   p
 })

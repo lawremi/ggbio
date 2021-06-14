b83109a2a0437bee6c264a936869eae45eb446a9
Sanchit Saini <sanchitsaini14@gmail.com>
2021-06-15 01:36:48 +0530

    Remove error message as ggbio depends on biovizBase

--- R/transformations.R
+++ R/transformations.R
@@ -3,15 +3,10 @@ stepping <- new_generic("stepping", signature = "data")
 method(stepping, "GRanges") <- function(data, group, extend.size = 0,
                                         fix = "center", group.selfish = TRUE,
                                         ...) {
-    if(requireNamespace("biovizBase", quietly = TRUE)) {
-        args <- list(data, group.selfish = group.selfish, fix = fix,
-                     extend.size = extend.size)
-        if (!missing(group))
-            args <- c(args, group = group)
-        data <- do.call(biovizBase::addStepping, args)
-    } else {
-        stop("Package biovizBase required to perfom stepping transform, please install it.",
-             .Call = FALSE)
-    }
+    args <- list(data, group.selfish = group.selfish, fix = fix,
+                 extend.size = extend.size)
+    if (!missing(group))
+        args <- c(args, group = group)
+    data <- do.call(biovizBase::addStepping, args)
     data
 }

2bd101708c5b5432cf3eb878ef7d577f4de5b974
Sanchit Saini <sanchitsaini14@gmail.com>
2021-06-15 13:17:06 +0530

    Rewrite geom_bar()

--- R/geom_bar-method.R
+++ R/geom_bar-method.R
@@ -1,40 +1,29 @@
 setGeneric("geom_bar", function(data, ...) standardGeneric("geom_bar"))
 
-setMethod("geom_bar", "ANY", function(data, ...){
-  ggplot2::geom_bar(data  = data, ...)
+setMethod("geom_bar", "ANY", function(data, ...) {
+    ggplot2::geom_bar(data  = data, ...)
 })
 
 ## alignment should be convenient toggle with chevron...
-setMethod("geom_bar", "GRanges", function(data,..., xlab, ylab, main){
-
-  args <- list(...)
-  args.aes <- parseArgsForAes(args)
-  args.non <- parseArgsForNonAes(args)
-  facet <- build_facet(data, args, facet_grid, facet_wrap)
-  if(length(data)){
-    if(!"y" %in% names(args.aes)){
-      if("score" %in% colnames(values(data))){
-        message("use score as y by default")
-        args.aes$y <- as.name("score")
-      }else{
-        stop("missing y values in aes(), or please provide a column named 'score'")
-      }
+setMethod("geom_bar", "GRanges",
+          function(data = NULL, mapping = NULL, ...,
+                   facets = ~seqnames,
+                   xlab = "Genomic Coordinates",
+                   ylab, main) {
+    if(!"y" %in% names(mapping)) {
+        if("score" %in% colnames(values(data))) {
+            message("use score as y by default")
+            mapping$y <- as.name("score")
+        } else {
+            stop("missing y values in aes(), or please provide a column named 'score'",
+                 call. = FALSE)
+        }
     }
-    .y <- quo_name(args.aes$y)
-    if (missing(ylab))
-      ylab <- .y
-    args.aes <- remove_args(args.aes, "y")
-    args.aes$xmin <- as.name("start")
-    args.aes$xmax <- as.name("end")
-    args.aes$ymin <- 0
-    args.aes$ymax <- as.name(.y)
-    aes.res <- do.call(aes, args.aes)
-    p <- list(do.ggcall(geom_rect, c(list(data = mold(data)), list(aes.res), args.non)))
-  }else{
-    p <- NULL
-  }
-  p <- c(p, list(facet))
-  labels <- Labels(xlab, ylab, main, fallback = c(x = ""))
-  p <- c(p, labels)
-  p
+
+    y <- quo_name(mapping$y)
+    mapping <- aes(xmin = start, xmax = end, ymin = 0, ymax = !!as.name(y))
+
+    c(geom_rect(fortify(data), mapping, ...),
+      facet_grid(facets),
+      Labels(xlab, ylab, main, fallback = c(y = y)))
 })

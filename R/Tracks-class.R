54a6a8e2339daef29722fee5a156aa952de81e70
Sanchit Saini <sanchitsaini14@gmail.com>
2021-10-05 19:21:48 +0530

    Move methods working with grid from utils to Tracks
    - Move getLegendGrob() and arrangeGrobByParsingLegend()

--- R/Tracks-class.R
+++ R/Tracks-class.R
@@ -1038,4 +1038,34 @@ setMethod("[", c("Tracks", "numeric", "missing", "ANY"),
                   label.width = x@label.width)
           })
 
+getLegendGrob <- function(p){
+  if(is(p, "GGbio"))
+    p <- p@ggplot
+  g <- ggplotGrob(p)
+  gg <- gtable_filter(g, "guide-box")
+}
+
+arrangeGrobByParsingLegend <- function(..., nrow = NULL, ncol = NULL,
+                                       widths = c(4, 1), legend.idx = NULL){
+  lst <- list(...)
+  if(length(lst) == 1 && is.list(lst[[1]]))
+    lst <- lst[[1]]
+
+  gg <- lapply(lst, getLegendGrob)
+
+  l.g <- lapply(lst, function(x){
+    x <- x + theme(legend.position = "none", aspect.ratio = 1)
+    if(is(x, "GGbio"))
+      res <- ggplotGrob(x@ggplot)
+    else
+      res <- ggplotGrob(x)
+    res
+  })
+
+  if(!is.null(legend.idx))
+    gg <- gg[legend.idx]
+  gg2 <- do.call(arrangeGrob, c(gg, list(ncol = 1)))
+  print(grid.arrange(do.call(arrangeGrob, c(l.g, list(nrow = nrow, ncol = ncol))),
+                     gg2, ncol = 2, widths = widths))
+}
 
